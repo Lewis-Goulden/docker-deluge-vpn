85d3341f123753c574b6e1b66a5812831a6be69b
lewisalexandergoulden <lewis.goulden@protonmail.com>
2017-12-09 12:54:06 +0000

    Add routes

--- root/etc/openvpn/startvpn.sh
+++ root/etc/openvpn/startvpn.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
+route add -net 192.168.1.0 netmask 255.255.255.0 gw 172.17.0.1 dev eth0
+route add -net 172.16.0.0 netmask 255.240.0.0 gw 172.17.0.1 dev eth0
+route add -net 10.0.0.0 netmask 255.0.0.0 gw 172.17.0.1 dev eth0
 /usr/sbin/openvpn --config /etc/openvpn/vpn.ovpn
